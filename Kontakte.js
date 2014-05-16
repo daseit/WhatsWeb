b5b5a59df3d6107c517e35f73698a2915efa12ca
Benjamin Rupp <beruit01@hs-esslingen.de>
2014-05-16 08:38:56 +0200

    feat(Kontakte): create template file for Kontakte templates and modify gitignore Add .meteor to .gitignore to prevent git to uploading meteor stuff.

--- Kontakte.js
+++ Kontakte.js
@@ -10,6 +10,8 @@ if (Meteor.isClient) {
         console.log("You pressed the button");
     }
   });*/
+
+  
   Session.set('currentPage', 'chatpage');
   tMain(Template.main);
   var AppRouter = Backbone.Router.extend({
@@ -38,13 +40,42 @@ if (Meteor.isClient) {
   Meteor.startup(function () {
     Backbone.history.start({ pushState: true });
   });
+
+ //contact list
+Contacts = new Meteor.Collection('contacts');
+
+//add new contact
+addContact = function(newName){
+	console.log("button pushed");
+	//console.log(newName);
+	var contact = {
+		name: newName,
+		//email: newEmail,
+		//pw: newPw,
+	  };
+	  Contacts._collection.insert(contact);
+	
+} 
+
+
+Template.contact_list.contacts = function(){
+ if (Contacts.find().count() > 0)
+		 {
+			var contacts = Contacts.find({}, {}).fetch();
+			console.log(contacts.length);
+			for (var i = 0; i < contacts.length; ++i ) {
+				console.log(contacts[i].name);
+			//	if (i > 0 && contacts[i].name == contacts[contacts.length-1].name && i != (contacts.length-1) )
+			//		console.log("Message already exists => " + contacts[i].name);
+				//document.getElementById('result').value = contacts[i].name; 	
+			}
+		 }
+	return contacts;
+} 
 }
 
 if (Meteor.isServer) {
   Meteor.startup(function () {
     // code to run on server at startup
-  });
-  
-  
-  
+  });  
 }
